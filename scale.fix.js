54cbc62d6a703a90b640e9c186519ac4f1327080
Bassi Renata <54715443+rbassii@users.noreply.github.com>
2024-12-04 12:44:47 -0300

    Update scale.fix.js

--- scale.fix.js
+++ scale.fix.js
@@ -16,4 +16,24 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     }, false);
   }
+
+  // Lógica do Dark Mode
+  const toggleButton = document.getElementById('dark-mode-toggle');
+
+  // Checa se o Dark Mode foi ativado previamente no localStorage
+  if (localStorage.getItem('dark-mode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+  }
+
+  // Alterna o Dark Mode ao clicar no botão
+  toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Salva a preferência no localStorage
+    if (document.body.classList.contains('dark-mode')) {
+      localStorage.setItem('dark-mode', 'enabled');
+    } else {
+      localStorage.setItem('dark-mode', 'disabled');
+    }
+  });
 });
